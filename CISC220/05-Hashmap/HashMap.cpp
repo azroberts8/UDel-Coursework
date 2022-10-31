ab4f6ffb724322c2cfeee6770f49369a6e55ac54
azroberts8 <aroberts8@usf.edu>
2022-10-31 19:11:11 -0400

    First commit; DOES NOT COMPILE! Lots of debugging of Yarrington's code to get this to compile

---
+++ CISC220/05-Hashmap/HashMap.cpp
@@ -0,0 +1,43 @@
+
+
+#include "HashMap.hpp"
+#include "HashNode.hpp"
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+hashMap::hashMap(bool hash1, bool coll1) {
+}
+void hashMap::addKeyValue(string k, string v) {
+}
+int hashMap::getIndex(string k) {
+}
+
+int hashMap::calcHash2(string k){
+}
+int hashMap::calcHash1(string k){
+}
+void hashMap::getClosestPrime() {
+}
+void hashMap::reHash() {
+}
+int hashMap::coll1(int h, int i, string k) {
+}
+int hashMap::coll2(int h, int i, string k) {
+}
+
+void hashMap::printMap() {
+	cout << "In printMap()" << endl;
+	for (int i = 0; i < mapSize; i++) {
+		//cout << "In loop" << endl;
+		if (map[i] != NULL) {
+			cout << map[i]->keyword << ": ";
+			for (int j = 0; j < map[i]->currSize;j++) {
+				cout << map[i]->values[j] << ", ";
+			}
+			cout << endl;
+		}
+	}
+}
+
+
---
+++ CISC220/05-Hashmap/README.md
@@ -0,0 +1,26 @@
+# CISC220 HashMap Project
+
+The fith project of the semester in CISC220. For this map we will be implementing a basic hashmap.
+
+### Authors:
+- Andrew Roberts (andrew@azroberts.com)
+- John Serino (jhserino@udel.edu)
+
+## Basic Setup
+
+For this project we have prepared a Makefile that that uses GCC to compile for *NIX systems. Make sure you have GCC installed on your system before proceeding with these steps!
+
+To compile and run the program use:
+```bash
+make run
+```
+
+To only compile the program without running use:
+```bash
+make all
+```
+
+To remove the compiled output:
+```bash
+make clean
+```
