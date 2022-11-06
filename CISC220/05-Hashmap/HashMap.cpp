c7f738b377a5108b5e40d1a57aac6dd377fc7b37
azroberts8 <aroberts8@usf.edu>
2022-11-06 15:06:19 -0500

    Completed collision functions, modified reHash()

--- CISC220/05-Hashmap/HashMap.cpp
+++ CISC220/05-Hashmap/HashMap.cpp
@@ -17,6 +17,7 @@ hashMap::hashMap(bool hash1, bool coll1) {
 	// Array of 14 primes where each prime is at least 2x previous starting at 97 (used in getClosestPrime())
 	primeSizes = new int[97, 197, 397, 797, 1597, 3203, 6421, 12853, 25717, 51437, 102877, 205759, 411527, 823117];
 }
+
 void hashMap::addKeyValue(string k, string v) {
 	int index = getIndex(k);
 	if (map[index]->keyword == k){
@@ -31,14 +32,12 @@ void hashMap::addKeyValue(string k, string v) {
 		reHash();
 	}
 }
+
 int hashMap::getIndex(string k) {
-	int index = 0;
-	if (hashfn){
-		index = calcHash1(k);
-	}
-	else{
-		index = calcHash2(k);
-	}
+	int index;
+	if(hashfn) index = calcHash1(k);
+	else index = calcHash2(k);
+
 	if(map[index]->keyword!=k && map[index] != NULL){
 		hashcoll = hashcoll + 1;
 		if (collfn){
@@ -75,31 +74,61 @@ void hashMap::getClosestPrime() {
 }
 
 void hashMap::reHash() {
-	//create new hashmap to store values
-	hashMap *reMap = new hashMap(hashfn, collfn);
-	reMap->mapSize = mapSize;
-	reMap->getClosestPrime();
-	reMap->map = new hashNode* [reMap->mapSize];
-	//fill new array with NULL values
-	for(int i = 0; i < reMap->mapSize; i++){
-		reMap->map[i]=NULL;
-	}
-	//duplicate old values into new HashMap
-	for(int i = 0; i < mapSize; i++){
-		if (map[i] != NULL){
-			int index2 = reMap->getIndex(map[i]->keyword);
-			reMap->map[index2] = map[i];
-		}
-	}
-	//re-initialize old map to new map created and delete the new map
-	map = reMap->map;
-	mapSize = reMap->mapSize;
-	delete reMap;
+	// copy old map and size into temporary variables
+	int oldSize = mapSize;
+	hashNode** oldMap = map;
 
+	// increment mapSize and generate new map
+	getClosestPrime();
+	map = new hashNode*[mapSize];
+
+	// copy old values to new map
+	int index;
+	for(int i = 0; i < oldSize; i++) {
+		if(oldMap[i] == NULL) continue;
+
+		if(hashfn) index = calcHash1(oldMap[i]->keyword);
+		else index = calcHash2(oldMap[i]->keyword);
+
+		map[index] = oldMap[i];
+	}
+
+	delete oldMap;
 }
+
 int hashMap::coll1(int h, int i, string k) {
+	/*
+	Only using h and k, not sure what i is needed for;
+	Casts previously computed hash to a char, appends it to the end
+	of the key and hashes this new string. If the newly calculated
+	space is NULL or it's key matches the current key; it returns the
+	hash. Otherwise, it recursively calls itself with the last hash
+	*/
+
+	// cut the used hash into a char and append it to the end of the key
+	string newKey = k + (char)h;
+
+	// hash the new key
+	int newHash;
+	if(hashfn) newHash = calcHash1(newKey);
+	else newHash = calcHash2(newKey);
+
+	if(map[newHash] == NULL || map[newHash]->keyword == k) return newHash;
+	else return coll1(newHash, 0, k);
 }
+
 int hashMap::coll2(int h, int i, string k) {
+	/*
+	Only using h and k;
+	Using probing with incrementation by 1 until space is NULL or
+	key is equal to current key;
+	*/
+
+	while(map[h] != NULL && map[h]->keyword != k) {
+		h++;
+	}
+
+	return h;
 }
 
 void hashMap::printMap() {
