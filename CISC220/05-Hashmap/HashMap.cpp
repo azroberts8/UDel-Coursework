81f30a0bbbd95fc61505cce954f266131db0a37e
unknown <jhserino@udel.edu>
2022-11-06 12:29:34 -0500

    addKeyValue and getIndex methods being worked on

--- CISC220/05-Hashmap/HashMap.cpp
+++ CISC220/05-Hashmap/HashMap.cpp
@@ -18,9 +18,40 @@ hashMap::hashMap(bool hash1, bool coll1) {
 	primeSizes = new int[97, 197, 397, 797, 1597, 3203, 6421, 12853, 25717, 51437, 102877, 205759, 411527, 823117];
 }
 void hashMap::addKeyValue(string k, string v) {
-
+	int index = getIndex(k);
+	if (map[index]->keyword == k){
+		map[index]->addValue(v);
+	}
+	else if (map[index]==NULL){
+		map[index] = new hashNode(k);
+		map[index]->addValue(v);
+		numKeys = numKeys + 1;
+	}
+	if (numKeys/(mapSize * 1.0) >= .7){
+		reHash();
+	}
 }
 int hashMap::getIndex(string k) {
+	int index = 0;
+	if (hashfn){
+		index = calcHash1(k);
+	}
+	else{
+		index = calcHash2(k);
+	}
+	if(map[index]->keyword!=k && map[index] != NULL){
+		hashcoll = hashcoll + 1;
+		if (collfn){
+			index = coll1(index,1,k);
+			collisions = collisions - 1;
+		}
+		else{
+			index = coll2(index,1,k);
+			collisions = collisions - 1;
+		}
+	}
+	return index;
+
 }
 
 int hashMap::calcHash2(string k){
